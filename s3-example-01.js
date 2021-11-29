9ade52ef24e4acf4be02bbdc1c66e81e09e58984
Michael Shuff <mjshuff23@gmail.com>
2021-11-29 12:04:03 -0500

    added bucket/object creation sample

--- s3-example-01.js
+++ s3-example-01.js
@@ -1,3 +1,34 @@
 // Load the SDK and UUID
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
+
+// Create a unique bucket name
+const bucketName = `node-sdk-sample-${uuid.v4()}`;
+// Create name for uploaded object key
+const keyName = `helloWorld.txt`;
+
+// Create a promise on an S3 service object
+const bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+  .createBucket({ Bucket: bucketName })
+  .promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise
+  .then((data) => {
+    // Create params for putObject call
+    const objectParams = {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: 'Hello, world!',
+    };
+
+    // Create object upload promise
+    const uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+      .putObject(objectParams)
+      .promise();
+
+    uploadPromise.then((data) =>
+      console.log(`Successfully uploaded data to ${bucketName}/${keyName}`)
+    );
+  })
+  .catch((err) => console.error(err, err.stack));
